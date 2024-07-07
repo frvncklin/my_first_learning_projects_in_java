948554146f0c80342730fce3495a9cb813da23aa
Victor Francklin <118057237+frvncklin@users.noreply.github.com>
2024-07-07 02:27:52 -0300

    Update Temperatura.java

--- Temperatura.java
+++ Temperatura.java
@@ -4,7 +4,7 @@ public class Temperatura {
 
 	public static void main(String[] args) {
 		double temperaturaCelsius = 30.5;
-		final double AJUSTEFHT = 32.0;
+		final int AJUSTEFHT = 32;
 		final double FATORFHT = 9/5.0;
 		double temperaturaFahrenheit = (temperaturaCelsius + AJUSTEFHT) * FATORFHT;
 		System.out.println("A temperatura, em ºF, equivalente a " + temperaturaCelsius + "ºC é " + temperaturaFahrenheit + "ºF.");
